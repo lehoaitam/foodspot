827b102fb6e52296a504e49509f0896ad54d80e1
lehoaitam <lehoaitam@gmail.com>
2016-08-06 23:13:26 -0500

    create a web server

--- Test/main.go
+++ Test/main.go
@@ -1,133 +1,29 @@
 package main
+
 import (
 	"fmt"
-	"time"
-
-	"math"
+	"net/http"
+	"strings"
+	"log"
 )
-type I interface {
-	M()
-}
-type MyFloat float64
-type Vertex struct{
-	X,Y float64
-}
-func (f MyFloat) M() {
-	fmt.Println("method of MyFloat")
-}
-func (v *Vertex) M(){
-	fmt.Println("method of Vertext")
-}
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func (p Person) String() string{
-	return fmt.Sprintf("age : %v - name : %v",p.Age,p.Name)
-}
-type IPAddr [4]byte
-
-func (ip IPAddr) String() string{
-	return fmt.Sprintf("%v.%v.%v.%v", ip[0],ip[1],ip[2],ip[3])
-}
-type MyError struct{
-	When time.Time
-	What string
-}
-func (err *MyError) Error() string{
-	return fmt.Sprintf("at %v, %s", err.When, err.What)
-}
-func run() error{
-	err := MyError{
-		time.Now(),
-		"i didnot work",
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()  // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form)  // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
 	}
-	return &err
-}
-
-type ErrNegativeSqrt float64
-func (err ErrNegativeSqrt) Error() string{
-	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(err))
-}
-func Sqrt(f float64) (float64, error){
-	if f < 0 {
-		return 0, ErrNegativeSqrt(f)
-	}else{
-		return math.Sqrt(f), nil
-	}
-}
-
-func fibonacci() func() int {
-	x := 0
-	y := 1
-	return func() int{
-		x , y = y, x + y
-		return x
-	}
+	fmt.Fprintf(w, "Hello golang webserver!") // send data to client side
 }
 
 func main() {
-
-	//f := fibonacci()
-	//for i := 0; i < 10; i++{
-	//	fmt.Println(f())
-	//}
-
-	//hosts := map[string]IPAddr{
-	//	"home" : {127, 0, 0, 1},
-	//	"school" : {192, 2, 2, 8},
-	//}
-	//
-	//for key, value := range hosts{
-	//	fmt.Printf("%v - %v\n", key, value)
-	//}
-	//
-	err := run()
-	if err != nil{
-		fmt.Println(err)
+	http.HandleFunc("/", sayhelloName) // set router
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
-	fmt.Println("test error")
-	//
-	//fmt.Println(Sqrt(2))
-	//fmt.Println(Sqrt(-2))
-	//s := []int{2, 3, 5, 7, 11, 13}
-	//printSlice(s)
-	//
-	//// Slice the slice to give it zero length.
-	//s = s[:0]
-	//printSlice(s)
-	//
-	//// Extend its length.
-	//s = s[:4]
-	//printSlice(s)
-	//
-	//// Drop its first two values.
-	//s = s[2:]
-	//printSlice(s)
-	//
-	//result := make([][]uint8, 3)
-	//for i := 0; i < 3; i++ {
-	//	result[i] = make([]uint8,8)
-	//	for j := 0; j < 8; j++ {
-	//		result[i][j] = uint8(i * j)
-	//	}
-	//}
-	//fmt.Println("2d: ", result)
-	//
-	//fmt.Println("finished configure")
-	//fmt.Println("")
-
-
-
-
-}
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
-}
-
-func describe ( i interface{}){
-	fmt.Printf("(%v - %T)\n", i , i)
-	//fmt.Printf("(%v, %T)\n", i, i)
 }
