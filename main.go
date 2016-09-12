1f3ad4f652c4d6ecc09591d1111619d62849b105
nqtrung <nqtgalaxy@gmail.com>
2016-09-12 20:53:52 +0700

    Signed-off-by: nqtrung <nqtgalaxy@gmail.com>

--- main.go
+++ main.go
@@ -3,7 +3,24 @@ package main
 import (
 	_ "foodspot/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+	"fmt"
 )
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("mysqluser") + ":" + beego.AppConfig.String("mysqluser") + "@/" + beego.AppConfig.String("mysqldb") + "?charset=utf8")
+	orm.SetMaxIdleConns("default", 30)
+	orm.DefaultTimeLoc = time.UTC
+
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func main() {
 	beego.Run()
