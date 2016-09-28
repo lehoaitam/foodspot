4c1000fd503f7690a72f95351a854fae132c62e9
tdduy88 <tdduy88@gmail.com>
2016-09-28 18:12:25 +0700

    Sử dụng Modern Business Bootstrap theme làm front end https://startbootstrap.com/template-overviews/modern-business/

--- controllers/frontend/top.go
+++ controllers/frontend/top.go
@@ -13,3 +13,69 @@ func (c *TopController) Get() {
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "frontend/index.html"
 }
+
+// Demo frontend template
+
+func (c *TopController) Page404Demo() {
+	c.TplName = "frontend/404.html"
+}
+
+func (c *TopController) AboutDemo() {
+	c.TplName = "frontend/about.html"
+}
+
+func (c *TopController) BlogHome1Demo() {
+	c.TplName = "frontend/blog-home-1.html"
+}
+
+func (c *TopController) BlogHome2Demo() {
+	c.TplName = "frontend/blog-home-2.html"
+}
+
+func (c *TopController) BlogPostDemo() {
+	c.TplName = "frontend/blog-post.html"
+}
+
+func (c *TopController) ContactDemo() {
+	c.TplName = "frontend/contact.html"
+}
+
+func (c *TopController) FaqDemo() {
+	c.TplName = "frontend/faq.html"
+}
+
+func (c *TopController) FullWidthDemo() {
+	c.TplName = "frontend/full-width.html"
+}
+
+func (c *TopController) Portfolio1ColDemo() {
+	c.TplName = "frontend/portfolio-1-col.html"
+}
+
+func (c *TopController) Portfolio2ColDemo() {
+	c.TplName = "frontend/portfolio-2-col.html"
+}
+
+func (c *TopController) Portfolio3ColDemo() {
+	c.TplName = "frontend/portfolio-3-col.html"
+}
+
+func (c *TopController) Portfolio4ColDemo() {
+	c.TplName = "frontend/portfolio-4-col.html"
+}
+
+func (c *TopController) PortfolioItemDemo() {
+	c.TplName = "frontend/portfolio-item.html"
+}
+
+func (c *TopController) PricingDemo() {
+	c.TplName = "frontend/pricing.html"
+}
+
+func (c *TopController) ServicesDemo() {
+	c.TplName = "frontend/services.html"
+}
+
+func (c *TopController) SideBarDemo() {
+	c.TplName = "frontend/sidebar.html"
+}
--- routers/router.go
+++ routers/router.go
@@ -1,15 +1,36 @@
 package routers
 
 import (
-	"foodspot/controllers/frontend"
 	"foodspot/controllers/backend"
+	"foodspot/controllers/frontend"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &frontend_controllers.TopController{})
+	beego.Router("/", &frontend_controllers.TopController{})
 	beego.Router("/login", &backend_controllers.LoginController{}, "get,post:Login")
 	beego.Router("/logout", &backend_controllers.LoginController{}, "get:Logout")
 	beego.Router("/signup", &backend_controllers.LoginController{}, "get,post:Signup")
 	beego.Router("/backoffice/categories", &backend_controllers.CategoriesController{})
+
+	// demo frontend template
+	beego.Router("/index", &frontend_controllers.TopController{})
+	beego.Router("/404", &frontend_controllers.TopController{}, "get:Page404Demo")
+	beego.Router("/about", &frontend_controllers.TopController{}, "get:AboutDemo")
+	beego.Router("/about", &frontend_controllers.TopController{}, "get:AboutDemo")
+	beego.Router("/blog-home-1", &frontend_controllers.TopController{}, "get:BlogHome1Demo")
+	beego.Router("/blog-home-2", &frontend_controllers.TopController{}, "get:BlogHome2Demo")
+	beego.Router("/blog-post", &frontend_controllers.TopController{}, "get:BlogPostDemo")
+	beego.Router("/contact", &frontend_controllers.TopController{}, "get:ContactDemo")
+	beego.Router("/faq", &frontend_controllers.TopController{}, "get:FaqDemo")
+	beego.Router("/full-width", &frontend_controllers.TopController{}, "get:FullWidthDemo")
+	beego.Router("/portfolio-1-col", &frontend_controllers.TopController{}, "get:Portfolio1ColDemo")
+	beego.Router("/portfolio-2-col", &frontend_controllers.TopController{}, "get:Portfolio2ColDemo")
+	beego.Router("/portfolio-3-col", &frontend_controllers.TopController{}, "get:Portfolio3ColDemo")
+	beego.Router("/portfolio-4-col", &frontend_controllers.TopController{}, "get:Portfolio4ColDemo")
+	beego.Router("/portfolio-item", &frontend_controllers.TopController{}, "get:PortfolioItemDemo")
+	beego.Router("/pricing", &frontend_controllers.TopController{}, "get:PricingDemo")
+	beego.Router("/services", &frontend_controllers.TopController{}, "get:ServicesDemo")
+	beego.Router("/sidebar", &frontend_controllers.TopController{}, "get:SideBarDemo")
 }
