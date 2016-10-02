0e1a1e8bdc4614d2a700633f819a746dd2d7f650
lehoaitam <lehoaitam@gmail.com>
2016-10-02 21:59:53 +0700

    using typescript

---
+++ static/js/categories.component.ts
@@ -0,0 +1,9 @@
+import { Component,enableProdMode } from '@angular/core';
+
+//enableProdMode();
+
+@Component({
+  selector: 'categories-app',
+  template: '<h1>Categories</h1>'
+})
+export class AppComponent { }
---
+++ static/js/categories.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './categories.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
